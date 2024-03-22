c0f44e0aa6f9033e248d630e62f1d1c3bcb877a3
parvat07 <bhusalparvat@gmail.com>
2024-03-22 00:58:00 +0000

    added top button

--- script1.js
+++ script1.js
@@ -1,21 +1,20 @@
 
-function calculateTotal() {
-    let totalResults = [0, 0, 0, 0];
-
-    for (let i = 1; i <= 11; i++) {
-      for (let j = 1; j <= 4; j++) {
-        const answer = parseInt(document.getElementById(`q${i}a${j}`).value) || 0;
-        totalResults[j - 1] += answer;
-      }
+  // Get the button:
+  let mybutton = document.getElementById("myBtn");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
     }
-
-    const totalResultsText = `
-      Total for Answer 1: ${totalResults[0]}<br>
-      Total for Answer 2: ${totalResults[1]}<br>
-      Total for Answer 3: ${totalResults[2]}<br>
-      Total for Answer 4: ${totalResults[3]}
-    `;
-
-    document.getElementById('resultContainer').innerHTML = totalResultsText;
   }
-
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
